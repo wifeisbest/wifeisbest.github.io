6d52bc29eb7b4001ca8ae919110932a38a06ae60
Duong Nguyen <duongf5vcu@gmail.com>
2021-04-03 08:40:10 +0700

    done

--- js/main.js
+++ js/main.js
@@ -24,4 +24,42 @@ function linkAction () {
     navMenu.classList.remove('show');
 }
 
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click',linkAction));
+
+// scoll
+const sr = ScrollReveal({
+    origin: 'top',
+    distance : '80px',
+    duration: 2000,
+    reset: true
+})
+// home
+sr.reveal('.home__title',{})
+sr.reveal('.home__img',{delay: 400})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home__social-icon',{interval: 200})
+// about
+
+sr.reveal('.about__img',{})
+sr.reveal('.about__subtitle',{delay: 200})
+sr.reveal('.about__text',{delay: 400})
+
+// skills
+sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay: 200})
+sr.reveal('.skills__img',{delay: 400})
+sr.reveal('.skills__data',{interval: 200})
+
+// work
+sr.reveal('.work__img',{interval: 200})
+
+// contact
+sr.reveal('.contact__input',{interval: 200})
+
+
+
+
+
+
+
+
