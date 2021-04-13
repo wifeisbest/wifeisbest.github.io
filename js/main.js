df798030dc77366b4e978a01f59d90e8ca159eae
Duong Nguyen <duongf5vcu@gmail.com>
2021-04-13 08:13:26 +0700

    remove transitions scroll mouse

--- js/main.js
+++ js/main.js
@@ -34,27 +34,27 @@ const sr = ScrollReveal({
     reset: true
 })
 // home
-sr.reveal('.home__title',{})
-sr.reveal('.home__img',{delay: 400})
-sr.reveal('.button',{delay: 200})
-sr.reveal('.home__social-icon',{interval: 200})
-// about
+// sr.reveal('.home__title',{})
+// sr.reveal('.home__img',{delay: 400})
+// sr.reveal('.button',{delay: 200})
+// sr.reveal('.home__social-icon',{interval: 200})
+// // about
 
-sr.reveal('.about__img',{})
-sr.reveal('.about__subtitle',{delay: 200})
-sr.reveal('.about__text',{delay: 400})
+// sr.reveal('.about__img',{})
+// sr.reveal('.about__subtitle',{delay: 200})
+// sr.reveal('.about__text',{delay: 400})
 
-// skills
-sr.reveal('.skills__subtitle',{})
-sr.reveal('.skills__text',{delay: 200})
-sr.reveal('.skills__img',{delay: 400})
-sr.reveal('.skills__data',{interval: 200})
+// // skills
+// sr.reveal('.skills__subtitle',{})
+// sr.reveal('.skills__text',{delay: 200})
+// sr.reveal('.skills__img',{delay: 400})
+// sr.reveal('.skills__data',{interval: 200})
 
-// work
-sr.reveal('.work__img',{interval: 200})
+// // work
+// sr.reveal('.work__img',{interval: 200})
 
-// contact
-sr.reveal('.contact__input',{interval: 200})
+// // contact
+// sr.reveal('.contact__input',{interval: 200})
 
 
 
